7477c720e2db3c598dc5de8c44e28e9f33ce3010
javarcho <alanvarcho@gmail.com>
2022-01-18 16:48:47 -0600

    mvp

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
+
+import MovieList from './components/MovieList';
+import Movie from './components/Movie';
+
+import MovieHeader from './components/MovieHeader';
+
+import AddMovieForm from './components/AddMovieForm';
+import FavoriteMovieList from './components/FavoriteMovieList';
+
+const App = props => {
+  const displayFavorites = true;
+  console.log("props from App: ", props)
+
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/>Redux Module Project</span>
+      </nav>
+
+      <div className="container">
+        <MovieHeader/>
+        <div className="row ">
+          {props.favorites.displayFavorites && <FavoriteMovieList/>}
+        
+          <Switch>
+            <Route exact path="/movies/add">
+              <AddMovieForm />
+            </Route>
+
+            <Route path="/movies/:id">
+              <Movie />
+            </Route>
+
+            <Route path="/movies">
+              <MovieList/>
+            </Route>
+
+            <Route path="/">
+              <Redirect to="/movies"/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state)=>{
+  return{
+    favorites: state.favorites
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider} from 'react-redux';
 
-import reducer from './reducers';
+import combineReducer from './reducers';
 
-import App from './components/App';
+import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+const store = createStore(combineReducer)
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
